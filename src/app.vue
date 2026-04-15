46a85e45cef35f4994ad006fad8762475f546e80
Emanuel Chipana huarancca. <u202214074@upc.edu.pe>
2026-04-15 10:53:21 -0500

    feat(developer): add developer display component and integrate it into app.vue.

--- src/app.vue
+++ src/app.vue
@@ -1,39 +1,34 @@
 <script setup>
-
-
 import {ref} from "vue";
 import {Developer} from "./greetings/domain/model/developer.entity.js";
+import DeveloperRegistration from "./greetings/presentation/components/developer-registration.vue";
 import DeveloperCountShow from "./greetings/presentation/components/developer-count-show.vue";
 
-const  firstName =ref('');
-const lastName =ref('');
+const firstName = ref('');
+const lastName = ref('');
 const developerCount = ref(0);
 const hasRegistered = ref(false);
 
-
 function updateRegisteredDeveloperInfo(developer) {
-
   firstName.value = developer.firstName;
   lastName.value = developer.lastName;
   hasRegistered.value = true;
-  console.log('Developer registered', developer);
+  console.log('Developer registered: ', developer);
+  updateDeveloperCount(developer);
 }
 
 function resetRegisteredDeveloperInfo() {
   firstName.value = '';
   lastName.value = '';
   hasRegistered.value = false;
-  console.log('Registered reset');
-
-
+  console.log('Registration deferred');
 }
 
-
-function updateDeveloperInfo(developer) {
-
-  const dev= new Developer(developer.firstname, developer.lastname);
-  if (dev.fullName !== 'Unknown') developer.Count.value++;
+function updateDeveloperCount(developer) {
+  const dev = new Developer(developer.firstName, developer.lastName);
+  if (dev.fullName !== 'Unknown') developerCount.value++;
 }
+
 </script>
 
 <template>
@@ -41,7 +36,11 @@ function updateDeveloperInfo(developer) {
   <h1>Hello Vue Developer Application</h1>
     <developer-registration
      @developer-registration="updateRegisteredDeveloperInfo"
-     @developer-deferred="resetRegisteredDeveloperInfo"
+     @registration-deferred-deferred="resetRegisteredDeveloperInfo"
     />
+    <developer-greeting v-if="hasRegistered" :first-name="firstName" :last-name="lastName"/>
     <developer-count-show : developer-count="developerCount"/>
+
+
+
 </template>
