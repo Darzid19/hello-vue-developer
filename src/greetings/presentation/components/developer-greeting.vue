5cc1f1f330e37c57eebb1f70d06a1bb271cdb14a
Emanuel Chipana huarancca. <u202214074@upc.edu.pe>
2026-04-15 10:55:06 -0500

    feat(developer-greeting): add greetings componet to display personalized welcome message

---
+++ src/greetings/presentation/components/developer-greeting.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+  import {computed} from "vue";
+
+  const props = defineProps(['firstName', 'lastName']);
+
+
+  const greeting = computed(() => {
+
+    if (!pros.firstName?.trim() || !props.lastName?.trim())
+
+    return "Welcome Anonymous Developer";
+    const developer = new Developer(props.firstName, props.lastName);
+    return `Congrats ${developer.fullName}! Now you are a Vue Developer`;
+  });
+</script>
+
+<template>
+  <p>
+    {{greeting}}
+  </p>
+
+</template>
+
+
+
+
+<style scoped>
+
+</style>
